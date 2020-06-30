9637c9b2a3d8c1d162754b6a90128c055ae49990
unknown <ray_ramez@yahoo.com>
2020-06-30 09:54:03 -0700

    first push for my blog repo

---
+++ src/build/precache-manifest.37cd075bb96932b9b8407917c3fc984c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bb3ea60cc6fdb33923d603f6f6c556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a486f612ac35f8ebfe11",
+    "url": "/static/css/main.f5588194.chunk.css"
+  },
+  {
+    "revision": "782125352da1d9f5e6d6",
+    "url": "/static/js/2.9fef1150.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9fef1150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a486f612ac35f8ebfe11",
+    "url": "/static/js/main.ee2168d4.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
